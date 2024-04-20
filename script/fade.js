89888482a3d04acea2843d1fc97ac5b2b6cfda72
cosmic-pooh <65557174+cosmic-pooh@users.noreply.github.com>
2024-04-20 15:00:26 +0900

    24.4.20
    初回

---
+++ script/fade.js
@@ -0,0 +1,11 @@
+$('#fade').slick({
+    autoplay:true,
+    autoplaySpeed:2500,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    cssEase: 'linear',
+    arrows:false,
+    dots:false,
+  });
+
